0adba897d916f48c6c35b92f125039e6da8a5c99
Romeo Noro Guterres <romeo.noro@ufn.edu.br>
2025-04-02 15:53:24 -0300

    Update util.py

--- AlgoritmosGenericos/util.py
+++ AlgoritmosGenericos/util.py
@@ -2,12 +2,14 @@ import random
 
 class Util:
     letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+    tamanho = len(letras)
+    
     @staticmethod
     def gerar_palavra(tamanho_palavra):
         palavra = ''
-        for i in range(tamanho_palavra):
-            palavra += Util.letras[ posicao_sorteada ]
-
+        
+        for _ in range(tamanho_palavra):
+            posicao_sorteada = random.randrange(Util.tamanho)
+            palavra += Util.letras[posicao_sorteada]
+        
         return palavra
-    
-
